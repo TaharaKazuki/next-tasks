d21ea2ae5200b04a4f80ed6e3eeb681d02867238
Tahara Kazuki <>
2024-05-09 22:25:34 +0900

    update

---
+++ src/components/SideMenu/TaskCard/TaskDeleteButton/TaskDeleteButton.tsx
@@ -0,0 +1,20 @@
+import { FaTrashAlt } from 'react-icons/fa';
+
+type TaskDeleteButtonProps = {
+  id: string;
+};
+
+const TaskDeleteButton = ({ id }: TaskDeleteButtonProps) => {
+  return (
+    <form action="">
+      <button
+        type="submit"
+        className="hover:text-gray-700 text-lg cursor-pointer"
+      >
+        <FaTrashAlt />
+      </button>
+    </form>
+  );
+};
+
+export default TaskDeleteButton;
