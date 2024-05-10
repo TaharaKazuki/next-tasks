0fc4cc326b7885faf0aa7ddcdd78055509e93549
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-10 16:35:09 +0900

    update

--- src/app/(main)/completed/page.tsx
+++ src/app/(main)/completed/page.tsx
@@ -23,13 +23,6 @@ const CompletedTaskPage = async () => {
         <h1 className="text-2xl font-bold flex items-center">
           Completed Tasks
         </h1>
-        <Link
-          href="/new"
-          className="flex items-center gap-1 font-semibold border px-4 py-2 rounded-full shadow-sm text-white bg-gray-800 hover:bg-gray-700"
-        >
-          <MdAddTask className="size-5" />
-          <div>Add Task</div>
-        </Link>
       </header>
       <div className="mt-8 flex flex-wrap gap-4">
         {completedTasks.map((task) => (
---
+++ src/app/api/tasks/expired/route.ts
@@ -0,0 +1,34 @@
+import { TaskDocument, TaskModel } from '@/models/task';
+import { connectDb } from '@/utils/database';
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  const currentDate = new Date()
+    .toLocaleString('ja-JP', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    })
+    .replace(/\//g, '-');
+
+  try {
+    await connectDb();
+    const expiredTasks: TaskDocument[] = await TaskModel.find({
+      isCompleted: false,
+      dueDate: { $lt: currentDate },
+    });
+
+    return NextResponse.json({
+      message: '期限切れタスク取得成功',
+      tasks: expiredTasks,
+    });
+  } catch (error) {
+    console.info(error);
+    return NextResponse.json(
+      { message: '期限切れタスク取得失敗' },
+      { status: 500 }
+    );
+  }
+};
+
+export const dynamic = 'force-dynamic';
